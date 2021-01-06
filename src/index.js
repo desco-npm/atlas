1ac611d6f7239af218a4b09174396740e18bb46c
eudiasrafael <eu@diasrafael.com.br>
2021-01-06 20:39:56 -0300

    Cabeçalho

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const cliHeader = require('@desco/cli-header')
+
 require('./functions')
 require('./globals')
 
@@ -5,6 +7,8 @@ const Orm = require('./modules/ORM')
 const Server = require('./modules/Server')
 
 module.exports = async () => {
+  cliHeader({ title: 'AtlasJS v2.0.0', size: 25, align: 'center', })
+
   require('./env.js')()
 
   await Orm.init()
