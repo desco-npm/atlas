ee958c7603bf5ad475603e4f766bc153361bc8cc
eudiasrafael <eu@diasrafael.com.br>
2021-01-09 11:16:29 -0300

    Organizado TODO

--- src/modules/ORM.js
+++ src/modules/ORM.js
@@ -3,6 +3,9 @@ const { Sequelize, DataTypes, Op, } = require('sequelize')
 let sequelize
 
 //TODO: Validações no ORM - https://trello.com/c/k2vo9AWU/32-valida%C3%A7%C3%B5es-no-orm
+//TODO: Migrações - https://trello.com/c/pF6LJKPU/21-migra%C3%A7%C3%B5es
+//TODO: Transições - https://trello.com/c/J2l5Tvrj/22-transições
+//TODO: Login com redes sociais - https://trello.com/c/TM9vRY23/29-login-com-redes-sociais
 class ORM {
   constructor () {
     this.modelsDir = pathJoin(projectDir, 'models')
