161d49d25bbfcf6a72da5474d34e0d090f5f429e
eudiasrafael <eu@diasrafael.com.br>
2021-01-12 13:52:08 -0300

    TODO

--- src/modules/ORM.js
+++ src/modules/ORM.js
@@ -3,6 +3,7 @@ const { Sequelize, DataTypes, Op, } = require('sequelize')
 let sequelize
 
 //TODO: Validações no ORM - https://trello.com/c/k2vo9AWU/32-valida%C3%A7%C3%B5es-no-orm
+//TODO: Implementar "Paranoid" no Sequelize do ORM - https://trello.com/c/4I4CZukx/38-implementar-paranoid-no-sequelize-do-orm
 //TODO: Migrações - https://trello.com/c/pF6LJKPU/21-migra%C3%A7%C3%B5es
 //TODO: Transições - https://trello.com/c/J2l5Tvrj/22-transições
 //TODO: Login com redes sociais - https://trello.com/c/TM9vRY23/29-login-com-redes-sociais
