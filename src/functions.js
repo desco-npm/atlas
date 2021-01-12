4cda8a100ac3360569b2276848c5095f52c2901e
eudiasrafael <eu@diasrafael.com.br>
2021-01-12 17:23:14 -0300

    PDF

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,13 @@
 global.clone = require('clone')
 global.fileExists = require('fs-extra').existsSync
+global.readFileSync = require('fs-extra').readFileSync
 global.pathJoin = require('path').join
 global.objectFilter = require('object-filter')
 global.objectMap = require('object.map')
 global.readdir = require('fs-extra').readdir
 global.isArray = require('is-array')
 global.stackTrace = require('stack-trace')
+global.htmlPdf = require('html-pdf')
+global.pdfMake = require('pdfmake')
 
 global.arrayUnique = array => array.filter((item, key, self) => self.indexOf(item) === key)
