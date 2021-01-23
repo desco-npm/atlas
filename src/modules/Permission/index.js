b553d8d419c1f59daf54d147c734097a1f770fbd
eudiasrafael <eu@diasrafael.com.br>
2021-01-23 16:37:38 -0300

    config de propriedade do permission

--- src/modules/Permission/index.js
+++ src/modules/Permission/index.js
@@ -6,13 +6,19 @@ class Permission {
 
   async init () {
     const express = Atlas.Server.express()
-    const User = Atlas.Orm.listModels()[process.env.Atlas.PERMISSION_USER_MODEL]
-    const Group = Atlas.Orm.listModels()[process.env.Atlas.PERMISSION_GROUP_MODEL]
-    const Permission = Atlas.Orm.listModels()[process.env.Atlas.PERMISSION_MODEL]
+    const User = Atlas.Orm.listModels()[process.env.Atlas.AUTH_USER_MODEL]
+    const Group = Atlas.Orm.listModels()[process.env.Atlas.AUTH_GROUP_MODEL]
+    const Permission = Atlas.Orm.listModels()[process.env.Atlas.AUTH_MODEL]
     const secret = process.env.Atlas.AUTH_SECRET
     const google = process.env.Atlas.GOOGLE_AUTH
     const algorithm = process.env.Atlas.AUTH_ALGORITHM
     const throughUserGroup = [ User.name, Group.name, ].sort().join('_')
+    const pswProp = process.env.Atlas.AUTH_PSW_PROP
+    const loginProp = process.env.Atlas.AUTH_LOGIN_PROP
+    const tokenProp = process.env.Atlas.AUTH_TOKEN_PROP
+    const tokenTypeProp = process.env.Atlas.AUTH_TOKEN_TYPE_PROP
+    const userPkProp = process.env.Atlas.AUTH_USER_PK_PROP
+    const expireTokenProp = process.env.Atlas.AUTH_EXPIRE_TOKEN_PROP
 
     Atlas.newEntity(throughUserGroup)
 
@@ -24,6 +30,12 @@ class Permission {
       secret,
       google,
       algorithm,
+      pswProp,
+      loginProp,
+      tokenProp,
+      tokenTypeProp,
+      userPkProp,
+      expireTokenProp,
     })
 
     return Promise.resolve()
