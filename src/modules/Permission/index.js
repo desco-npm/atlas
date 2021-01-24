7517bd002f8267d5a46f911e55247068fc9d6748
eudiasrafael <eu@diasrafael.com.br>
2021-01-24 13:20:29 -0300

    Mais env para google

--- src/modules/Permission/index.js
+++ src/modules/Permission/index.js
@@ -11,10 +11,15 @@ class Permission {
     const Permission = Atlas.Orm.listModels()[process.env.Atlas.AUTH_MODEL]
     const secret = process.env.Atlas.AUTH_SECRET
     const google = process.env.Atlas.GOOGLE_AUTH
+    const googleId = process.env.Atlas.GOOGLE_AUTH_ID
+    const googleKey = process.env.Atlas.GOOGLE_AUTH_KEY
+    const googleScope = process.env.Atlas.GOOGLE_AUTH_SCOPE
+    const googlePrompt = process.env.Atlas.GOOGLE_AUTH_PROMPT
     const algorithm = process.env.Atlas.AUTH_ALGORITHM
     const throughUserGroup = [ User.name, Group.name, ].sort().join('_')
     const pswProp = process.env.Atlas.AUTH_PSW_PROP
     const loginProp = process.env.Atlas.AUTH_LOGIN_PROP
+    const mailProp = process.env.Atlas.AUTH_MAIL_PROP
     const tokenProp = process.env.Atlas.AUTH_TOKEN_PROP
     const tokenTypeProp = process.env.Atlas.AUTH_TOKEN_TYPE_PROP
     const userPkProp = process.env.Atlas.AUTH_USER_PK_PROP
@@ -29,9 +34,14 @@ class Permission {
       Permission,
       secret,
       google,
+      googleId,
+      googleKey,
+      googleScope,
+      googlePrompt,
       algorithm,
       pswProp,
       loginProp,
+      mailProp,
       tokenProp,
       tokenTypeProp,
       userPkProp,
