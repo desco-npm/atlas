ef7171b10ff701984deaab948ebb2220a78d4e31
eudiasrafael <eu@diasrafael.com.br>
2021-04-29 18:55:04 -0300

    2.8.0 - Rotas padrões não usam mais o CRUD/

--- src/modules/Server/mixin/CRUD.js
+++ src/modules/Server/mixin/CRUD.js
@@ -1,34 +1,34 @@
 module.exports = ({ express, entity, Model, models, }) => {
   if (!Model) return
 
-  express.get(`/crud/${entity}/`, async (req, res) => {
+  express.get(`/${entity}/`, async (req, res) => {
     const query = frontToSequelize(req.query, models, Atlas.Orm.Sequelize())
 
     res.json(await Model.select(query))
   })
 
-  express.post(`/crud/${entity}/`, async (req, res) => {
+  express.post(`/${entity}/`, async (req, res) => {
     const data = req.body
     const query = frontToSequelize(req.query, models, Atlas.Orm.Sequelize())
 
     res.json(await Model.insert(data, query))
   })
 
-  express.get(`/crud/${entity}/:id`, async (req, res) => {
+  express.get(`/${entity}/:id`, async (req, res) => {
     const id = req.params.id
     const query = frontToSequelize(req.query, models, Atlas.Orm.Sequelize())
 
     res.json(await Model.read(id, query))
   })
 
-  express.put(`/crud/${entity}/:id`, async (req, res) => {
+  express.put(`/${entity}/:id`, async (req, res) => {
     const data = { id: req.params.id, ...req.body, }
     const query = frontToSequelize(req.query, models, Atlas.Orm.Sequelize())
 
     res.json(await Model.change(data, query))
   })
 
-  express.delete(`/crud/${entity}/:id`, async (req, res) => {
+  express.delete(`/${entity}/:id`, async (req, res) => {
     res.json(await Model.delete(req.params.id))
   })
 }
