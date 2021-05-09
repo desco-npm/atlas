3659068a137902d0fa327469f18099e84ece8306
DESKTOP-7082DFH\eu <eu@diasrafael.com.br>
2021-05-09 17:44:55 -0300

    2.9.1

--- src/dynamicImports.js
+++ src/dynamicImports.js
@@ -1,5 +1,6 @@
 module.exports = {
   '@desco/cli-header': () => require('@desco/cli-header'),
   '@desco/sequelize-permission-resources': () => require('@desco/sequelize-permission-resources'),
+  '@desco/front-to-sequelize': () => require('@desco/front-to-sequelize'),
   ...require('./dynamicImports_dev')
 }
--- src/dynamicImports_dev.js
+++ src/dynamicImports_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
   '@desco/cli-header': () => require('../../cli-header'),
   '@desco/sequelize-permission-resources': () => require('../../sequelize-permission-resources'),
+  '@desco/front-to-sequelize': () => require('../../front-to-sequelize'),
 }
