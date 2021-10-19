186a6abc15e7ef5135d64f72ba5dc968ee526cb7
Rafael Dias <eu@diasrafael.com.br>
2021-10-19 10:45:46 -0300

    dotEnv

--- js/Mail/dictionary.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** Dictionary of the AtlasJS Mail Module */
-exports.default = {
-    en: {},
-    ptbr: {}
-};
---
+++ ts/lib/dotEnv/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Dotenv is a zero-dependency module that loads environment variables from a .env file into
+ * process.env. Storing configuration in the environment separate from code is based on The
+ * Twelve-Factor App methodology.
+ * 
+ * Third Party Resource
+ * 
+ * NPM: https://www.npmjs.com/package/dotenv
+ * 
+ */
+
+import dotEnv from 'dotenv'
+
+export default dotEnv
